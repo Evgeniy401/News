9fe041c1d55769f3cfb3022943fbf9012ad2e6a7
Admin <Evgeniy@>
2025-10-05 16:53:11 +0300

    work on registration

--- app/src/main/java/com/example/myapplication/presentation/ui/component/Button.kt
+++ app/src/main/java/com/example/myapplication/presentation/ui/component/Button.kt
@@ -1,6 +1,28 @@
 package com.example.myapplication.presentation.ui.component
 
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
-//@Composable
-//fun StyledButton
+
+@Composable
+fun StyledButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable () -> Unit
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        shape = RoundedCornerShape(size = 15.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Black
+        )
+    ) {
+        content
+    }
+}
